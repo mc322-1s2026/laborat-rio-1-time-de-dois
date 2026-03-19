7c9d83002da2a50f53cc69006b9077b3355299c4
joelvirtuose <joelvirtuose@yahoo.com.br>
2026-03-19 18:02:45 +0000

    Classe Project quase concluída

--- src/main/java/com/nexus/model/Project.java
+++ src/main/java/com/nexus/model/Project.java
@@ -1,5 +1,6 @@
 package com.nexus.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Project {
@@ -7,15 +8,36 @@ public class Project {
     private List<Task> taskList;
     private int totalBudget;
 
-    public Project(String projecstName, int totalBudget) {
-    projectName = projectName.trim();
+    // Construtor que demanda nome do projeto e orçamento total
+    public Project(String projectName, int totalBudget) {
     if (projectName == null || projectName.isBlank()) {
         throw new IllegalArgumentException("Nome do projeto não pode ser vazio.");
     }
     if (totalBudget <= 0) {
         throw new IllegalArgumentException("Orçamento total (em horas) deve ser inteiro positivo.");
     }
-    this.projectName = projectName;
+    this.projectName = projectName.trim();
     this.totalBudget = totalBudget;
+    this.taskList = new ArrayList<>();
+    }
+
+    // Adiciona tarefas no projeto até o limite do orçamento de horas
+    public void addTask(Task t) {
+    if (t == null) {
+        throw new IllegalArgumentException("Tarefa vazia.");
+    }
+    int currentEffort = taskList.stream()
+                    .mapToInt(Task::getEstimatedEffort)
+                    .sum();
+    int totalEffort = currentEffort + t.getEstimatedEffort();
+    if (totalEffort > totalBudget) {
+        throw new IllegalArgumentException("Orçamento total (em horas) do Projeto foi excedido.");
+    }
+    taskList.add(t);
+    }
+
+    // Permite obter uma cópia imutável da lista de tarefas do projeto 
+    public List<Task> getTaskList() {
+        return List.copyOf(taskList);
     }
 }
